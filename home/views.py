6c855d6316b62feef109257f4d235b6cb58d2409
devjadiya <121892398+devjadiya@users.noreply.github.com>
2023-11-01 21:41:39 +0530

    dubara updated

---
+++ home/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render,HttpResponse
+
+def home(request):
+    if request.method == 'POST':
+        my_input_value = request.POST.get('my_input', 'Default Value')
+    else:
+        my_input_value = 'Default Value'
+    
+    context = {
+        'variable': my_input_value
+    }
+    return render(request, 'index.html', context)
+
